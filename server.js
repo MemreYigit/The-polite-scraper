20b93f3b3057b4e745c89a103d304b7bfca19b95
Mustafa Emre Yigit <memrex14@gmail.com>
2026-08-18 15:08:33 +0300

    Initial setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).send('Health check passed');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
